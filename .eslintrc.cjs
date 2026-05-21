d5b74884fc1116618992cc8d2e778238e593e842
Kapil Chauhan <kapil.chauhan@procys.com>
2026-05-21 16:42:21 +0530

    chore(phase1): add lint, format, and vitest baseline configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module', project: ['./tsconfig.json'] },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+  ],
+  rules: {
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': ['error', { allow: ['error', 'warn'] }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.config.ts', 'scripts/', 'tests/', 'examples/'],
+};
